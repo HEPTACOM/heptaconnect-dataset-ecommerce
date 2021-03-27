a09d03317970cb0d90fbb8fc405b48bfa25e12e2
Joshua Behrens <behrens@heptacom.de>
2021-03-27 23:12:04 +0100

    Add missing collection type hints

--- src/Price/PriceCollection.php
+++ src/Price/PriceCollection.php
@@ -5,6 +5,9 @@ namespace Heptacom\HeptaConnect\Dataset\Ecommerce\Price;
 
 use Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection;
 
+/**
+ * @extends \Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection<\Heptacom\HeptaConnect\Dataset\Ecommerce\Price\Price>
+ */
 class PriceCollection extends DatasetEntityCollection
 {
     protected function getT(): string
