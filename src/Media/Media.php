80d49ec62af25fb965d49d240612e3e3cdb43403
Joshua Behrens <behrens@heptacom.de>
2021-05-22 22:10:21 +0200

    Add filename and title to media

--- src/Media/Media.php
+++ src/Media/Media.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Dataset\Ecommerce\Media;
 
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+use Heptacom\HeptaConnect\Dataset\Base\Translatable\TranslatableString;
 
 class Media extends DatasetEntityContract
 {
     protected string $normalizedStream = '';
 
     protected string $mimeType = '';
+
+    protected string $filename = '';
+
+    protected TranslatableString $title;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->title = new TranslatableString();
+    }
 
     public function getNormalizedStream(): ?string
     {
@@ -31,6 +42,30 @@ class Media extends DatasetEntityContract
     public function setMimeType(string $mimeType): Media
     {
         $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getTitle(): TranslatableString
+    {
+        return $this->title;
+    }
+
+    public function setTitle(TranslatableString $title): self
+    {
+        $this->title = $title;
 
         return $this;
     }
