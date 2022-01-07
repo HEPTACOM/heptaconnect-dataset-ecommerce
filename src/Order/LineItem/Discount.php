b9e8c3cd572bd0bb5a27611e74fba94ec34a4e95
Leon Rustmeier <l.rustmeier@heptacom.de>
2022-01-07 14:36:18 +0100

    Add discount percentage to discount line item

--- src/Order/LineItem/Discount.php
+++ src/Order/LineItem/Discount.php
@@ -8,4 +8,15 @@ use Heptacom\HeptaConnect\Dataset\Ecommerce\Order\LineItem;
 
 class Discount extends LineItem
 {
+    protected ?float $percentage = null;
+
+    public function getPercentage(): ?float
+    {
+        return $this->percentage;
+    }
+
+    public function setPercentage(?float $percentage): void
+    {
+        $this->percentage = $percentage;
+    }
 }
