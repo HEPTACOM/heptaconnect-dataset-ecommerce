01b847bc04a0db57cdb90ab2adb29b02309f02ed
Leon Rustmeier <l.rustmeier@heptacom.de>
2022-02-15 18:19:44 +0100

    Add payment status partially refunded

--- src/Order/PaymentState.php
+++ src/Order/PaymentState.php
@@ -16,6 +16,8 @@ class PaymentState extends DatasetEntityContract
 
     public const STATE_REFUNDED = 'e824b95c-f419-4322-bc68-bbee2389253a';
 
+    public const STATE_PARTIALLY_REFUNDED = 'aaa395f0-ba96-4fc3-8d0c-15eea84716a3';
+
     public const STATE_CANCELLED = 'dd77ecc7-b5be-45f4-85ee-27d6c2b68c9d';
 
     protected string $state = self::STATE_UNKNOWN;
