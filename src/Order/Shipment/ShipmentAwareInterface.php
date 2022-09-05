9d3e602787e851e86e67108b89dfc6a85b75039e
Michael Benz <benz@heptacom.de>
2022-09-05 17:03:20 +0200

    Add ShipmentAwareInterface

---
+++ src/Order/Shipment/ShipmentAwareInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Ecommerce\Order\Shipment;
+
+interface ShipmentAwareInterface
+{
+    public function getShipment(): Shipment;
+
+    public function setShipment(Shipment $shipment): void;
+}
